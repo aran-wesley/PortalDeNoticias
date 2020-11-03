027fc3c8f385604cff53ce05265c60a0a93f85cd
Aran Wesley S Marques <52837253+aran-wesley@users.noreply.github.com>
2020-11-03 16:08:16 -0300

    Iniciando

---
+++ PortalDeNoticias/Models/Noticia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PortalDeNoticias.Models
+{
+    public class Noticia
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        //[DisplayFormat(DataFormatString = "0:yyyy-MM-dd")]
+        //[DataType(DataType.DateTime, ErrorMessage = "Data em formato inválido")]
+        public DateTime Data { get; set; }
+        public string Conteudo { get; set; }
+    }
+}
---
+++ PortalDeNoticias/Models/RepositorioNoticia.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalDeNoticias.Models
+{
+    public class RepositorioNoticia
+    {
+        private static List<Noticia> noticias;
+
+        public static List<Noticia> Noticias
+        {
+            get
+            {
+                if (noticias == null)
+                    CriarNoticia();
+                return noticias;
+            }
+        }
+
+        private static void CriarNoticia()
+        {
+            noticias = new List<Noticia>();
+
+            noticias.Add(new Noticia
+            {
+                Id = 1,
+                Titulo = "Lorem ipsum dolor sit amet",
+                Autor = "João Calors",
+                Data = DateTime.Today,
+                Conteudo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus congue efficitur. Morbi eget lorem eleifend, consectetur sapien non, rutrum eros. Integer dictum ipsum eu vehicula pellentesque. Integer ullamcorper magna aliquam tempor eleifend. Ut sed lobortis ante. Pellentesque finibus laoreet tristique. Integer lobortis, orci quis maximus eleifend, sapien est sodales purus, vitae mattis dui risus suscipit quam."
+            });
+            noticias.Add(new Noticia
+            {
+                Id = 2,
+                Titulo = "Lorem ipsum dolor sit amet",
+                Autor = "Pedro Alvares",
+                Data = DateTime.Today.AddDays(-10),
+                Conteudo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus congue efficitur. Morbi eget lorem eleifend, consectetur sapien non, rutrum eros. Integer dictum ipsum eu vehicula pellentesque. Integer ullamcorper magna aliquam tempor eleifend. Ut sed lobortis ante. Pellentesque finibus laoreet tristique. Integer lobortis, orci quis maximus eleifend, sapien est sodales purus, vitae mattis dui risus suscipit quam."
+            });
+            noticias.Add(new Noticia
+            {
+                Id =3,
+                Titulo = "Lorem ipsum dolor sit amet",
+                Autor = "Lucas Pinto",
+                Data = DateTime.Today.AddDays(-2),
+                Conteudo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus congue efficitur. Morbi eget lorem eleifend, consectetur sapien non, rutrum eros. Integer dictum ipsum eu vehicula pellentesque. Integer ullamcorper magna aliquam tempor eleifend. Ut sed lobortis ante. Pellentesque finibus laoreet tristique. Integer lobortis, orci quis maximus eleifend, sapien est sodales purus, vitae mattis dui risus suscipit quam."
+            });
+            noticias.Add(new Noticia
+            {
+                Id = 4,
+                Titulo = "Lorem ipsum dolor sit amet",
+                Autor = "Aran Wesley",
+                Data = DateTime.Today.AddDays(-1),
+                Conteudo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus congue efficitur. Morbi eget lorem eleifend, consectetur sapien non, rutrum eros. Integer dictum ipsum eu vehicula pellentesque. Integer ullamcorper magna aliquam tempor eleifend. Ut sed lobortis ante. Pellentesque finibus laoreet tristique. Integer lobortis, orci quis maximus eleifend, sapien est sodales purus, vitae mattis dui risus suscipit quam."
+            });
+            noticias.Add(new Noticia
+            {
+                Id = 5,
+                Titulo = "Lorem ipsum dolor sit amet",
+                Autor = "Andrew Marques",
+                Data = DateTime.Today.AddDays(-5),
+                Conteudo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus congue efficitur. Morbi eget lorem eleifend, consectetur sapien non, rutrum eros. Integer dictum ipsum eu vehicula pellentesque. Integer ullamcorper magna aliquam tempor eleifend. Ut sed lobortis ante. Pellentesque finibus laoreet tristique. Integer lobortis, orci quis maximus eleifend, sapien est sodales purus, vitae mattis dui risus suscipit quam."
+            });
+
+        }
+    }
+}
